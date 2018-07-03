e7d67728beaf727133fac556c165352dde73a4fe
sean1130 <39747545+sean1130@users.noreply.github.com>
2018-07-02 22:36:37 -0600

    build library and schedule components and put them on the grid

---
+++ src/components/library/library.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Library extends Component {
+    render() {
+        return (
+            <div className="library">
+                <h1 clasName="library__title">Course Library</h1>
+            </div>
+
+        )
+    }
+}
+
+export default Library;
