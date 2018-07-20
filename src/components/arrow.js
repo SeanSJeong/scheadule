d378c88ab3da18adb7fcffe0ee797bc296092564
sean1130 <39747545+sean1130@users.noreply.github.com>
2018-07-19 21:47:03 -0600

    styled add remove button

--- src/components/arrow.js
+++ src/components/arrow.js
@@ -10,20 +10,18 @@ class Arrow extends Component {
         }
     }
 
-    toggleArrow = function () {
+    toggleArrow = function() {
         this.props.callback(this.state.status)
         if(this.state.status) {
-            // close it
             document.getElementById(this.id).classList.remove('arrow-closed');
         } else {
-            // open it
             document.getElementById(this.id).classList.add('arrow-closed');
         }
-       
-        this.setState({ status: !this.state.status})
+
+        this.setState({ status: !this.state.status })
     }.bind(this);
 
-    render () {
+    render() {
         this.id = `arrow-${this.props.id}`
         return (
             <a id={this.id} onClick={() => this.toggleArrow()} className={`${this.props.className} arrow`}></a>
